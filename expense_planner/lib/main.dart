3a138d0bacfdc75c6eca33afea4e5a2bba2d1d8e
jakubit0 <kubaleszczyniak@gmail.com>
2023-03-11 23:59:17 +0100

    Updated main
    Added Card in main and created new class Transaction

--- expense_planner/lib/main.dart
+++ expense_planner/lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
+import 'transaction.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  });
 
- 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -25,7 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -33,34 +33,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
+  final List<Transaction> transaction = [
+    Transaction(
+      id: 't1',
+      title: 'New shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(
-      
         title: const Text('Expense Planner'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-         children:  [
+        children: [
           Container(
             width: double.infinity,
             child: const Card(
-            color: Colors.blue,
-            elevation: 5,
-            child: Text('CHART'),
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('CHART'),
+            ),
           ),
+          Column(
+            children: transaction.map((tx) {
+              return Card(
+                child: Row(
+                  children: [
+                      Container(
+                        child: Text(tx.amount.toString()
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text(tx.title),
+                          Text(tx.date.toString()),
+                        ],
+                      )
+                  ],
+                ),
+              );
+            }).toList(),
           ),
-          const Card(
-            color: Colors.red,
-            child: Text('LIST OF TXT'),
-          )
-        ],       
-      ),    
+        ],
+      ),
     );
   }
 }
