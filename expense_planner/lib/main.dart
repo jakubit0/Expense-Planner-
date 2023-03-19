ddb119f6985b8320759cf14763ef15c3d28830b5
jakubit0 <kubaleszczyniak@gmail.com>
2023-03-19 15:47:25 +0100

    updated main
    styling containers

--- expense_planner/lib/main.dart
+++ expense_planner/lib/main.dart
@@ -72,45 +72,42 @@ class _MyHomePageState extends State<MyHomePage> {
               return Card(
                 child: Row(
                   children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                          vertical: 10, 
-                          horizontal: 15),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.purple,
-                              width: 2
-                              ),
-                              ),
-                              padding: const EdgeInsets.all(10),
-                        child: Text(
-                          '\$${tx.amount}',
-                          style: const TextStyle(
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2),
+                      ),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        '\$${tx.amount}',
+                        style: const TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
                           color: Colors.purple,
                         ),
-                        ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            tx.title,
-                            style: const TextStyle(                        
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tx.title,
+                          style: const TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.bold,
-                          ),),
-                          Text(
-                            DateFormat.yMMMd().format(tx.date),
-                            style: const TextStyle(
+                          ),
+                        ),
+                        Text(
+                          DateFormat.yMMMd().format(tx.date),
+                          style: const TextStyle(
                             color: Colors.grey,
                             fontSize: 16,
                             fontWeight: FontWeight.bold,
                           ),
-                          ),
-                        ],
-                      )
+                        ),
+                      ],
+                    )
                   ],
                 ),
               );
