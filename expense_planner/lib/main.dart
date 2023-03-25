32490978350f9cb60639dea3adeac959ff7c1f5c
jakubit0 <kubaleszczyniak@gmail.com>
2023-03-25 22:31:18 +0100

    more theme styling

--- expense_planner/lib/main.dart
+++ expense_planner/lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
       title: 'Personal Expenses',
       theme: ThemeData(
         primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
       ),
       home: MyHomePage(),
       debugShowCheckedModeBanner: false,
